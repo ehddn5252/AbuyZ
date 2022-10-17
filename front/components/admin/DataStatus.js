0e5fdbc972fd709c138c0fdac63918f2592dbbd0
권도건 <kjmk1007@naver.com>
2022-10-17 11:14:23 +0900

    :check: merge Test

--- front/components/admin/DataStatus.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function DataStatus() {
-  return (
-    <div>
-      <h1>진행 현황</h1>
-    </div>
-  );
-}
---
+++ front/components/admin/dogeon.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ReviewStatus() {
+  return (
+    <div>
+      <h1>도건</h1>
+    </div>
+  );
+}
