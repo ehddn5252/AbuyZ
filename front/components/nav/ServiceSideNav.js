c1636980fa6e6930aa78aa3edfc39498cb33cd8f
최지은 <zzieunchoi0707@gmail.com>
2022-10-18 15:06:12 +0900

    :sparkles: 고객센터 네브바

--- front/components/nav/ServiceSideNav.js
+++ front/components/nav/ServiceSideNav.js
@@ -1,13 +1,67 @@
 import React from "react";
+export default function ServiceSideNav(props) {
+  const tap0Change = () => {
+    props.setServiceTap(0);
+  };
+  const tap1Change = () => {
+    props.setServiceTap(1);
+  };
 
-export default function MypageSideNav() {
   return (
-    <div style={{ display: "grid" }}>
-      <h1> 고객센터사이드Nav </h1>
-      <span>쇼핑 FAQ</span>
-      <span>1:1 문의하기</span>
+    <div>
+      <div style={sidebarWrapper}>
+        <div style={sidebarMenu}>
+          <h3 style={sidebarTitle}>고객센터</h3>
+          <ul style={sidebarList}>
+            <li style={sidebarListItem}>
+              <button
+                style={{ border: "none", backgroundColor: "white" }}
+                onClick={tap0Change}
+              >
+                쇼핑 FAQ
+              </button>
+            </li>
+            <li style={sidebarListItem}>
+              <button
+                style={{ border: "none", backgroundColor: "white" }}
+                onClick={tap1Change}
+              >
+                1:1문의
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
       <span>잇다 고객센터</span>
       <span>1899 - 7000</span>
     </div>
   );
 }
+
+const sidebarWrapper = {
+  padding: "20px",
+  color: "black",
+};
+
+const sidebarMenu = {
+  marginBottom: "2rem",
+};
+
+const sidebarTitle = {
+  fontSize: "3rem",
+  color: "rgb(197, 197, 197)",
+};
+
+const sidebarList = {
+  listStyle: "none",
+  padding: "0.5rem",
+};
+
+const sidebarListItem = {
+  padding: "1rem",
+  cursor: "pointer",
+  display: "flex",
+  alignItems: "center",
+  borderRadius: "10px",
+  fontSize: "2rem",
+};
