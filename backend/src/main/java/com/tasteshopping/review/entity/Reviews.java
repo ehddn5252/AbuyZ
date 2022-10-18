75c54fd4c46e9d22a1203b07fcb080869f368b6a
박유주 <qkrdbwn98@naver.com>
2022-10-18 16:10:29 +0900

    :sparkles: review entity

--- backend/src/main/java/com/tasteshopping/review/entity/Reviews.java
+++ backend/src/main/java/com/tasteshopping/review/entity/Reviews.java
@@ -1,10 +1,43 @@
 package com.tasteshopping.review.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.tasteshopping.product.entity.Products;
+import com.tasteshopping.user.entity.Users;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 public class Reviews {
     @Id
-    Integer uid;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer uid;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="reviews_uid")
+    private Reviews parent_review;        //구매자 리뷰 유아이디
+
+    @Column(nullable = false)
+    private Timestamp date;             //작성날짜
+
+    @Column
+    private String content;             //작성내용
+
+    @Column(nullable = false)
+    private Float rating;               //평점
+
+    @Column
+    private String img_url;             //첨부사진
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="users_uid")
+    private Users user;          //사용자 유아이디
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="products_uid")
+    private Products product;       //상품 유아이디
+
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private Boolean report;              //신고여부
+
+
 }
