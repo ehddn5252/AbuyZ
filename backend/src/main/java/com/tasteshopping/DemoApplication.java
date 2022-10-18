1f8642bf62b7c92d1a284b163772dd437b5995b9
공지훈 <wlgns3914@naver.com>
2022-10-18 15:51:27 +0900

    [S07P31E201-69] :sparkles: Sign up & Set up spring security

--- backend/src/main/java/com/tasteshopping/DemoApplication.java
+++ backend/src/main/java/com/tasteshopping/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.tasteshopping.demo;
+package com.tasteshopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/main/java/com/tasteshopping/user/service/UserService.java
+++ backend/src/main/java/com/tasteshopping/user/service/UserService.java
@@ -1,4 +1,8 @@
 package com.tasteshopping.user.service;
 
+import com.tasteshopping.user.dto.ResponseDto;
+import com.tasteshopping.user.dto.UserDto;
+
 public interface UserService {
+    ResponseDto siginUp(UserDto userDto);
 }
