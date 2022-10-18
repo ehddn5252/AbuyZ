2eb206465768c495681cf9cb780b226229cf66c5
jangchangwan <tktkd6755@gmail.com>
2022-10-18 13:12:40 +0900

    :sparkles: passwordFind UI

--- front/pages/pwf.js
+++ front/pages/pwf.js
@@ -1,9 +1,75 @@
 import React from "react";
 
+// MUI
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
 export default function PasswordFind() {
   return (
-    <div>
-      <h1> 비밀번호 찾기 페이지</h1>
-    </div>
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        display: "flex",
+        height: "100vh",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        boxShadow={2}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "white",
+          borderRadius: "1rem",
+          padding: 4,
+          paddingTop: 0,
+        }}
+      >
+        <Typography component="h1" variant="h3" sx={{ color: "red" }}>
+          비밀번호 찾기
+        </Typography>
+        <Box
+          component="form"
+          noValidate
+          sx={{
+            mt: 1,
+          }}
+        >
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="이메일을 입력해주세요."
+            name="email"
+            autoComplete="email"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="name"
+            label="이름을 입력해주세요."
+            id="name"
+            autoComplete="name"
+          />
+
+          <Button
+            fullWidth
+            type="submit"
+            href="/login"
+            variant="contained"
+            color="error"
+            sx={{ mt: 1, mb: 3 }}
+          >
+            비밀번호 변경
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 }
