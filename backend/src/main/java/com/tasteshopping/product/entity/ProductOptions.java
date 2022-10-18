a4a8cc33afa085ae856ecff5cab5830ca0ea5f37
김동우 <sunbi5252@naver.com>
2022-10-18 15:18:32 +0900

    :sparkles: add base entity

--- backend/src/main/java/com/tasteshopping/product/entity/ProductOptions.java
+++ backend/src/main/java/com/tasteshopping/product/entity/ProductOptions.java
@@ -1,10 +1,19 @@
 package com.tasteshopping.product.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class ProductOptions {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Integer uid;
+
+    @Column(columnDefinition = "varchar(50)")
+    String name;
+
+    @Column(columnDefinition = "varchar(100)")
+    String description;
+
+    @Column(name="option_price")
+    Integer optionPrice;
 }
