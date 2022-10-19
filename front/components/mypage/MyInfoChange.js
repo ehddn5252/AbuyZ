94cce6f932e90ccfcfb1aaf4443b75e2a0ff8924
jangchangwan <tktkd6755@gmail.com>
2022-10-19 16:23:17 +0900

    :lipstick: 회원정보관리 UI 구현

--- front/components/mypage/MyInfoChange.js
+++ front/components/mypage/MyInfoChange.js
@@ -1,9 +1,133 @@
-import React from "react";
+// React
+import React, { useState } from "react";
+
+// MUI
+import Button from "@mui/material/Button";
+
+// StyledComponents
+import styled from "styled-components";
+
+// 하위 Components
+import PasswordChangeModal from "./PasswordChangeModal";
 
 export default function MyinfoChange() {
+  const [checkPhoneNumber, setCheckPhoneNumber] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const changePhoneNumber = () => {
+    if (checkPhoneNumber === false) {
+      setCheckPhoneNumber(true);
+    } else {
+      setCheckPhoneNumber(false);
+    }
+  };
+
+  const cancelPhoneNumber = () => {
+    setCheckPhoneNumber(false);
+  };
+
+  const showModal = () => {
+    setModalOpen(true);
+  };
   return (
-    <div>
-      <h1>MyinfoChange Component</h1>
-    </div>
+    <InfoContainer>
+      <MajorTitle>내 정보 관리</MajorTitle>
+      <hr style={{ height: "0.5rem", background: "#000" }} />
+      <ContentDiv>
+        <SubTitle>프로필 사진 수정</SubTitle>
+        <Button variant="outlined" component="label">
+          수정
+          <input hidden accept="image/*" multiple type="file" />
+        </Button>
+      </ContentDiv>
+      <hr />
+      <ContentDiv>
+        <SubTitle>전화번호 변경</SubTitle>
+        {checkPhoneNumber ? null : <SubContent>010-0592-3142</SubContent>}
+        {checkPhoneNumber ? (
+          <SubButton
+            variant="outlined"
+            color="success"
+            onClick={changePhoneNumber}
+          >
+            수정
+          </SubButton>
+        ) : (
+          <SubButton variant="outlined" onClick={changePhoneNumber}>
+            수정
+          </SubButton>
+        )}
+        {checkPhoneNumber ? (
+          <SubButton
+            variant="outlined"
+            color="error"
+            onClick={cancelPhoneNumber}
+          >
+            취소
+          </SubButton>
+        ) : null}
+      </ContentDiv>
+      <hr />
+      <ContentDiv>
+        <SubTitle>배송지 관리</SubTitle>
+        <SubContent>[489523] 부산 남구 우암로 193 SSAFY 201호실</SubContent>
+        <SubButton variant="outlined">수정</SubButton>
+      </ContentDiv>
+      <hr />
+      <ContentDiv>
+        <SubTitle>비밀번호 변경</SubTitle>
+        <SubButton variant="outlined" onClick={showModal}>
+          수정
+        </SubButton>
+        {modalOpen && (
+          <PasswordChangeModal
+            setModalOpen={setModalOpen}
+          ></PasswordChangeModal>
+        )}
+      </ContentDiv>
+      <hr />
+      <ContentDiv>
+        <SubTitle>회원 탈퇴</SubTitle>
+        <SubButton variant="outlined" color="error">
+          탈퇴
+        </SubButton>
+      </ContentDiv>
+      <hr />
+    </InfoContainer>
   );
 }
+
+const InfoContainer = styled.div`
+  margin-top: 10rem;
+  width: 100%;
+`;
+
+const MajorTitle = styled.h1`
+  font-size: 3rem;
+`;
+
+const ContentDiv = styled.div`
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  width: 90%;
+  height: 3rem;
+`;
+
+const SubTitle = styled.p`
+  padding: 0.5rem;
+  margin: 0;
+  font-size: 1.5rem;
+  width: 20rem;
+`;
+
+const SubContent = styled.p`
+  padding: 0;
+  margin: 0;
+  padding-right: 1rem;
+`;
+
+const SubButton = styled(Button)`
+  margin-right: 1rem;
+`;
