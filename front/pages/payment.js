94510c85d58f734de8532aff5192ffcc64ffe332
최지은 <zzieunchoi0707@gmail.com>
2022-10-19 10:59:36 +0900

    :sparkles: 결제와 상품 연결

--- front/pages/payment.js
+++ front/pages/payment.js
@@ -7,11 +7,31 @@ import ProductSimpleInfo from "../components/payment/ProductSimpleInfo";
 export default function Payment() {
   return (
     <div>
-      <h1> 결제 페이지</h1>
-      <MyShippingInfo />
-      <ProductSimpleInfo />
-      <ProductSaleInfo />
-      <PaymentProcess />
+      <div style={center}>
+        <h1> 주문 / 결제</h1>
+      </div>
+      <div style={card}>
+        <MyShippingInfo />
+      </div>
+      <div style={card}>
+        <ProductSimpleInfo />
+      </div>
+      <div style={card}>
+        <ProductSaleInfo />
+      </div>
+      <div style={card}>
+        <PaymentProcess />
+      </div>
     </div>
   );
 }
+
+const center = {
+  display: "flex",
+  justifyContent: "center",
+  marginTop: "3rem",
+};
+
+const card = {
+  padding: "5rem 20rem 5rem 20rem",
+};
