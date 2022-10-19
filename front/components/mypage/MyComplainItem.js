34e3176d6d8181f3ec83aaf5600cd7bc9c93737a
jangchangwan <tktkd6755@gmail.com>
2022-10-20 01:27:12 +0900

    :lipstick: 내문의내역 UI 구현

--- front/components/mypage/MyComplainItem.js
+++ front/components/mypage/MyComplainItem.js
@@ -1,9 +1,51 @@
+// React
 import React from "react";
 
-export default function MyComplainItem() {
+// StyledCopmoent
+import styled from "styled-components";
+
+export default function MyComplainItem({ complain }) {
   return (
-    <div>
-      <h1>MyComplainItem Component</h1>
-    </div>
+    <Container>
+      <h1>Q. {complain.title}</h1>
+      {complain.state ? (
+        <CompleteDiv>
+          <p>답변 완료</p>
+        </CompleteDiv>
+      ) : (
+        <InCompleteDiv>
+          <p>처리중</p>
+        </InCompleteDiv>
+      )}
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const CompleteDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ff9494;
+  color: white;
+  font-size: 1.5rem;
+  width: 8rem;
+  height: 3rem;
+  border-radius: 1rem;
+`;
+
+const InCompleteDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff5e4;
+  color: #ff9494;
+  font-size: 1.5rem;
+  width: 8rem;
+  height: 3rem;
+  border-radius: 1rem;
+`;
