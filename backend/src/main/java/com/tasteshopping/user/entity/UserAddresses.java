3a0e23cf20ec0efaab70bcbff81e2db346204e7a
공지훈 <wlgns3914@naver.com>
2022-10-25 17:57:24 +0900

    :sparkles: Create UserAddresses Entity

---
+++ backend/src/main/java/com/tasteshopping/user/entity/UserAddresses.java
@@ -0,0 +1,48 @@
+package com.tasteshopping.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAddresses {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int uid;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="users_uid")
+    Users user;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String detailAddress;
+
+    @Column(nullable = false)
+    private String postalCode;
+
+    @Column(nullable = false)
+    private String recipient;
+
+    @Column
+    private String contact1;
+
+    @Column
+    private String contact2;
+
+    @Column
+    private String note;
+
+
+}
