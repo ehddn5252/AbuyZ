725da060c47b9b27359ed15c71aec671c6096194
DESKTOP-KVCQHCD\SSAFY <tktkd6755@gmail.com>
2022-10-27 16:01:08 +0900

    :lipstick: basket UI

--- front/pages/basket.js
+++ front/pages/basket.js
@@ -1,9 +1,6 @@
 // React
 import React from "react";
 
-// MUI
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-
 // StyeldComponet
 import styled from "styled-components";
 
@@ -14,43 +11,25 @@ import BasketPayment from "../components/payment/BasketPayment";
 export default function Basket() {
   return (
     <Container>
+      <h1>장바구니</h1>
       <BasketBox>
-        <AddressBox>
-          <HomeOutlinedIcon sx={{ fontSize: "2rem", color: "#4185ED" }} />
-          <p style={{ color: "#4185ED", paddingRight: "1.2rem" }}>배송지</p>
-          <p>부산 남구 우암로 196, 삼성 청년 SW 아카데미 202호</p>
-        </AddressBox>
         <BasketList />
-      </BasketBox>
-      <PaymentBox>
         <BasketPayment />
-      </PaymentBox>
+      </BasketBox>
     </Container>
   );
 }
-
 const Container = styled.div`
   display: flex;
-  width: 100%;
-`;
-
-const BasketBox = styled.div`
-  width: 70%;
-`;
-
-const AddressBox = styled.div`
-  display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  border: 2px solid #aaaaaa;
-  border-radius: 1rem;
-  margin-top: 3rem;
-  margin-left: 6.5rem;
-  padding: 0.5rem;
-  width: 81%;
-  font-size: 1.2rem;
-  font-weight: bolder;
+  width: 55%;
+  margin: 0 22%;
+  padding-top: 1rem;
 `;
-
-const PaymentBox = styled.div`
-  width: 30%;
+const BasketBox = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
 `;
