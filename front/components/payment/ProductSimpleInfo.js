f7653d7e27fe555638f48c8df57c4dfe2dbb835a
최지은 <zzieunchoi0707@gmail.com>
2022-10-27 17:58:45 +0900

    :lipstick: 구매 창 상품 정보 ui

--- front/components/payment/ProductSimpleInfo.js
+++ front/components/payment/ProductSimpleInfo.js
@@ -1,47 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import styled from "styled-components";
 export default function ProductSimpleInfo() {
+  const [productList, setProductList] = useState(false);
+  const cartList = [
+    {
+      id: 0,
+      name: "싱싱한 햇살 당근",
+      options: "당근당근",
+      count: 3,
+      price: 15000,
+      img: "/images/carrot.png",
+      discount: 30,
+    },
+    {
+      id: 1,
+      name: "싱싱한 뭐시기",
+      options: "샌드위치샌드위치",
+      count: 7,
+      price: 2000,
+      img: "/images/sandwich.png",
+      discount: 10,
+    },
+  ];
   return (
     <div>
-      <h1>상품정보</h1>
+      <ProductShow onClick={() => setProductList(!productList)}>
+        <div>
+          <SubTitle>주문 상품</SubTitle>
+        </div>
+        <div>
+          <KeyboardArrowDownIcon></KeyboardArrowDownIcon>
+        </div>
+      </ProductShow>
       <hr></hr>
+      {productList ? (
+        <div>
+          {cartList.map((e) => (
+            <InfoDiv key={e.id}>
+              <ImgDiv>
+                <Img src={e.img}></Img>
+              </ImgDiv>
+              <TextDiv>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                  <NameOption>
+                    <span style={{ fontWeight: "bold", fontSize: "1.1rem" }}>
+                      {e.name}
+                    </span>
+                    <br></br>
+                    <span>[{e.options}]</span>
+                  </NameOption>
+                  <Count>
+                    <span>{e.count}개</span>
+                  </Count>
+                  <PriceDiv>
+                    <span
+                      style={{
+                        textDecoration: "line-through",
+                        color: "#aaaaaa",
+                      }}
+                    >
+                      {e.price}원
+                    </span>
+                    <br></br>
+                    <span style={{ fontWeight: "bold", fontSize: "1.1REM" }}>
+                      {(e.price * (100 - e.discount)) / 100}원
+                    </span>
+                  </PriceDiv>
+                </div>
+              </TextDiv>
+            </InfoDiv>
+          ))}
+        </div>
+      ) : (
+        <div
+          style={{ padding: "3rem", display: "flex", justifyContent: "center" }}
+        >
+          <span style={{ fontWeight: "bold" }}>{cartList[0].name} </span>
+          <span> 외 {cartList.length}개의 상품을 주문했습니다.</span>
+        </div>
+      )}
       {/* for문 돌려서 */}
-      <InfoDiv>
-        <ImgDiv>
-          <img
-            src={"/images/carrot.png"}
-            style={{ height: 150, width: 150 }}
-          ></img>
-        </ImgDiv>
-        <TextDiv>
-          <h1>싱싱한 햇살 당근</h1>
-          <div style={{ display: "flex" }}>
-            <CountDiv>
-              {/* 옵션 /로 끊어서 가져오기 */}
-              <span>옵션: 300g / 2입</span>
-              <br></br>
-              <br></br>
-              {/* 수량 가져오기 */}
-              <span>수량: 2개</span>
-            </CountDiv>
-            <PriceDiv>
-              {/* 총 가격 가져오기 */}
-              <PriceP>900원</PriceP>
-            </PriceDiv>
-          </div>
-        </TextDiv>
-      </InfoDiv>
     </div>
   );
 }
 
+const ProductShow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const SubTitle = styled.div`
+  font-weight: bold;
+  font-size: 1.5rem;
+`;
+
 const InfoDiv = styled.div`
   display: flex;
-  margin-top: 2rem;
+`;
+
+const Img = styled.img`
+  height: 6rem;
+  width: 4rem;
+  object-fit: cover;
 `;
 
 const ImgDiv = styled.div`
-  flex: 2;
+  flex: 1;
   margin-left: 1rem;
 `;
 
@@ -49,15 +115,17 @@ const TextDiv = styled.div`
   flex: 9;
 `;
 
-const CountDiv = styled.div`
+const NameOption = styled.div`
   flex: 7;
+  margin-top: 1.5rem;
+`;
+
+const Count = styled.div`
+  flex: 3;
+  margin-top: 2rem;
 `;
 
 const PriceDiv = styled.div`
   flex: 2;
-`;
-
-const PriceP = styled.div`
-  font-size: 3rem;
-  font-weight: bold;
+  margin-top: 1.5rem;
 `;
