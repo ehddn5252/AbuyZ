f47690d8760266e75beb6a812af1bd8bf58fcbcd
권도건 <kjmk1007@naver.com>
2022-10-28 12:07:04 +0900

    :sparkles: Coupon 페이지 구현

--- front/components/admin/coupon/CouponList.js
+++ front/components/admin/coupon/CouponList.js
@@ -1,61 +1,189 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import CouponItem from "./CouponItem";
-import Grid2 from "@mui/material/Unstable_Grid2";
 
-const coupon = [
-  {
-    title: "빼빼로 데이 1000원 쿠폰",
-    startDate: "2022.10.18 16:00",
-    endDate: "2022.11.11 12:00",
-    category: "식품",
-    discount: "1000",
-  },
-];
+// MUI
+import Grid2 from "@mui/material/Unstable_Grid2";
+import Box from "@mui/material/Box";
 
 export default function CouponList() {
+  const header = [
+    "수정",
+    "카테고리",
+    "쿠폰명",
+    "할인금액",
+    "시작 날짜",
+    "마감 날짜",
+  ];
+
+  const body = [
+    {
+      id: 0,
+      category: "생활/건강",
+      couponName: "나이키 청바지",
+      discount: 3000,
+      start: "2022.01.01",
+      end: "2022.01.30",
+    },
+  ];
+
   return (
-    <div>
-      <CouponHeaderBox>
-        <HeaderText>쿠폰 관리</HeaderText>
-        <AddCouponButton>쿠폰 등록</AddCouponButton>
-      </CouponHeaderBox>
-      <Grid2 container spacing={3}>
-        {coupon.map((data, idx) => (
-          <Grid2 xs={4} md={4} key={idx}>
-            <CouponItem coupon={data} />
-          </Grid2>
-        ))}
-      </Grid2>
-    </div>
+    <Grid2
+      xs={12}
+      sx={{
+        margin: "0",
+        marginTop: "2rem",
+        background: "white",
+        padding: "0",
+      }}
+    >
+      {/* <h2 style={{ paddingLeft: "2rem" }}>쿠폰 목록</h2>
+      <hr
+        style={{ background: "#ff9494", width: "95%", marginBottom: "2rem" }}
+      /> */}
+      <div
+        style={{
+          height: 500,
+          width: "90%",
+          display: "flex",
+          justifyContent: "center",
+          margin: "auto",
+          paddingBottom: "2rem",
+          border: "1px solid black",
+          marginBottom: "1rem",
+        }}
+      >
+        <TableContainer>
+          <thead>
+            <TableRow>
+              <Th>
+                <input
+                  type="checkbox"
+                  style={{ width: "1.5rem", height: "1.5rem" }}
+                />
+              </Th>
+              {header.map((e, idx) => (
+                <Th key={idx}>{e}</Th>
+              ))}
+            </TableRow>
+          </thead>
+          <tbody style={{ height: "50%" }}>
+            {body.map((e) => (
+              <TableRow key={e.id}>
+                <Td>
+                  <input
+                    type="checkbox"
+                    style={{ width: "1.5rem", height: "1.5rem" }}
+                  />
+                </Td>
+                <Td>
+                  <Edit>수정하기</Edit>
+                </Td>
+                <Td>{e.category}</Td>
+                <Td>{e.couponName}</Td>
+                <Td>{e.discount}</Td>
+                <Td>{e.start}</Td>
+                <Td>{e.end}</Td>
+              </TableRow>
+            ))}
+          </tbody>
+        </TableContainer>
+      </div>
+      <ButtonBox>
+        <DeleteButton>선택 삭제</DeleteButton>
+        <EditButton>수정 항목 저장</EditButton>
+      </ButtonBox>
+    </Grid2>
   );
 }
 
-const CouponHeaderBox = styled.div`
-  border: 0.3rem solid #ff9494;
-  border-radius: 1rem;
-  margin-bottom: 3rem;
+export const MyBox = styled(Box)`
+  /* width: 15rem; */
+  /* background-color: transparent; */
+  .MuiDataGrid-columnHeaders {
+    width: 100%;
+    background: #dadada;
+  }
+  .MuiDataGrid-columnHeaderDraggableContainer {
+    border: 1px;
+    border-color: #000;
+  }
+  .MuiDataGrid-columnHeaderTitleContainer {
+    background: #dadada;
+  }
+`;
+
+const TableContainer = styled.table`
+  background-color: white;
+  margin: 0;
+  width: 100%;
+  height: 10rem;
+  border-collapse: collapse;
+  border-spacing: 0;
+  border: 1px solid black;
+`;
+
+const TableRow = styled.tr`
+  width: 100%;
+  height: 3rem;
+  margin: 0;
+`;
+
+const Th = styled.th`
+  margin: 0;
+  border: 1px solid black;
+  text-align: center;
+  background-color: #c5e2ff;
+`;
+
+const Td = styled.td`
+  margin: 0;
+  border: 1px solid black;
+  text-align: center;
+  height: fit-content;
+  padding-top: 0.3rem;
+  padding-bottom: 0.3rem;
+`;
+
+const Edit = styled.button`
+  width: fit-content;
+  /* height: 2rem; */
+  background-color: #57a9fb;
+  font-size: 1rem;
+  color: white;
+  border: none;
+  padding: 0.6rem;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const ButtonBox = styled.div`
   display: flex;
-  justify-content: space-around;
-  align-items: center;
-  height: 5rem;
+  justify-content: center;
+  padding-bottom: 2rem;
+  background-color: white;
 `;
 
-const HeaderText = styled.p`
-  font-size: 1.5rem;
-  font-weight: 800;
+const DeleteButton = styled.button`
+  background-color: #ffffff;
+  color: black;
+  border: 1px solid;
+  height: 3rem;
+  width: content;
+  font-size: 1.3rem;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
-const AddCouponButton = styled.button`
-  background-color: #ff7171;
+const EditButton = styled.button`
+  background-color: #57a9fb;
   color: white;
   border: 1px solid;
-  border-radius: 0.8rem;
   margin-left: 1rem;
   height: 3rem;
-  width: 8rem;
+  width: fit-content;
   font-size: 1.3rem;
-  font-weight: 800;
   &:hover {
     cursor: pointer;
   }
