716fb6fb8fb099dce9441ea6339f08e60568c777
jangchangwan <tktkd6755@gmail.com>
2022-10-29 15:04:58 +0900

    :alien: 통계 api 추가

--- front/pages/api/statistics/hello.js
+++ front/pages/api/statistics/hello.js
@@ -1,5 +1,109 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import https from "../https.js";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+// 총 매출 조회
+export function getTotalsales(DateDto) {
+  // Header에 토큰 집어넣기
+  const accessToken = localStorage.getItem("access-Token");
+  https.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+  https
+    .post("/statistics/sales", {
+      start_date: DateDto.start_date,
+      end_date: DateDto.end_date,
+    })
+    .then((response) => {
+      if (response === 200) {
+        console.log("총 매출 조회 완료", response);
+        return response;
+      } else {
+        console.log("총 매출 조회 실패", response);
+        return response;
+      }
+    });
+}
+
+// 장바구니 통계 조회
+export function getCartStatistics(DateDto) {
+  // Header에 토큰 집어넣기
+  const accessToken = localStorage.getItem("access-Token");
+  https.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+  https
+    .get("/statistics/cart", {
+      start_date: DateDto.start_date,
+      end_date: DateDto.end_date,
+    })
+    .then((response) => {
+      if (response === 200) {
+        console.log("장바구니 통계 조회 완료", response);
+        return response;
+      } else {
+        console.log("장바구니 통계 조회 실패", response);
+        return response;
+      }
+    });
+}
+
+// 요일별 조회
+export function getDailyStatistics(DateDto) {
+  // Header에 토큰 집어넣기
+  const accessToken = localStorage.getItem("access-Token");
+  https.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+  https
+    .post("/statistics/daily", {
+      start_date: DateDto.start_date,
+      end_date: DateDto.end_date,
+    })
+    .then((response) => {
+      if (response === 200) {
+        console.log("요일별 조회 완료", response);
+        return response;
+      } else {
+        console.log("요일별 조회 실패", response);
+        return response;
+      }
+    });
+}
+// 카테고리별 백분율 조회
+export function getCategoryStatistics(DateDto) {
+  // Header에 토큰 집어넣기
+  const accessToken = localStorage.getItem("access-Token");
+  https.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+  https
+    .post("/statistics/category-percentage", {
+      start_date: DateDto.start_date,
+      end_date: DateDto.end_date,
+    })
+    .then((response) => {
+      if (response === 200) {
+        console.log("카테고리별 백분율 조회 완료", response);
+        return response;
+      } else {
+        console.log("카테고리별 백분율 조회 실패", response);
+        return response;
+      }
+    });
+}
+// 제품별 통계 조회
+export function getProductStatistics(DateDto) {
+  // Header에 토큰 집어넣기
+  const accessToken = localStorage.getItem("access-Token");
+  https.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+
+  https
+    .post("/statistics/product", {
+      start_date: DateDto.start_date,
+      end_date: DateDto.end_date,
+    })
+    .then((response) => {
+      if (response === 200) {
+        console.log("제품별 통계 조회 완료", response);
+        return response;
+      } else {
+        console.log("제품별 통계 조회 실패", response);
+        return response;
+      }
+    });
 }
