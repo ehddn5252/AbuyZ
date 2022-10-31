9cc06edbb5f3d3d00ce7db2e73358e537dd87dfd
DESKTOP-KVCQHCD\SSAFY <tktkd6755@gmail.com>
2022-10-31 18:02:23 +0900

    :aligns: login api 변경

--- front/pages/api/https.js
+++ front/pages/api/https.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from "axios";
+
 // axios 객체 생성
 export default axios.create({
   baseURL: "https://k7e201.p.ssafy.io/api",
