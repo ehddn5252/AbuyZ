7b51f2650fe3f7072a2dfd3bebbc939e62d0caad
박유주 <qkrdbwn98@naver.com>
2022-10-31 13:31:26 +0900

    :anchor: webconfig

--- backend/src/main/java/com/tasteshopping/DemoApplication.java
+++ backend/src/main/java/com/tasteshopping/DemoApplication.java
@@ -16,14 +16,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://k7e201.p.ssafy.io");
-			}
-		};
-	}
 }
