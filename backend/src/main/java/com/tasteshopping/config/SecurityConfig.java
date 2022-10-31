c1306e988ac875909bb12f829a3c70eaa9eea5eb
박유주 <qkrdbwn98@naver.com>
2022-10-31 17:35:55 +0900

    :anchor: security config

--- backend/src/main/java/com/tasteshopping/config/SecurityConfig.java
+++ backend/src/main/java/com/tasteshopping/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
@@ -53,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .antMatchers("/*").permitAll()
                 .anyRequest().permitAll()
                 .and()
