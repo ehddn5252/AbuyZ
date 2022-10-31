75969c33b34c9cfa12df73a6dd336b512b907ffd
DESKTOP-KVCQHCD\SSAFY <tktkd6755@gmail.com>
2022-10-31 09:51:32 +0900

    :alien: base api 수정

--- front/pages/api/https.js
+++ front/pages/api/https.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 // axios 객체 생성
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://k7e201.p.ssafy.io/api",
   headers: {
     "Content-type": "application/json",
   },
