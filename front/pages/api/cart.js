f30d1f8d53518a3fd2de8501663a5b0a290f62e3
최지은 <zzieunchoi0707@gmail.com>
2022-11-01 17:28:52 +0900

    :alien: cart api 변경

--- front/pages/api/cart.js
+++ front/pages/api/cart.js
@@ -2,63 +2,79 @@
 import https from "./https.js";
 
 // 장바구니에 담기
-// content type 넣는건 어떻게 하는거지?
-export function regiscart(cartDto) {
+export async function regiscart(cartDto) {
   const accessToken = localStorage.getItem("access-Token");
   https.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
 
-  https
-    .post("/cart", {
-      products_uid: cartDto.products_uid,
-      product_count: cartDto.product_count,
-      option_values: {
-        size: cartDto.size,
-        color: cartDto.color,
-      },
-    })
-    .then((response) => {
-      if (response === 200) {
-        console.log("장바구니에 담기 완료", response);
-        return response;
-      } else {
-        console.log("장바구니에 담기 실패", response);
-        return response;
-      }
-    });
+  return new Promise((resolve) => {
+    https
+      .post("/cart", {
+        products_uid: cartDto.products_uid,
+        product_count: cartDto.product_count,
+        option_values: {
+          size: cartDto.size,
+          color: cartDto.color,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("장바구니에 담기 완료", response);
+          resolve(response.data);
+        } else {
+          console.log("장바구니에 담기 실패", response);
+          resolve(response);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
 }
 
 // 장바구니 목록 가져오기
-export function cartlist() {
+export async function cartlist() {
   const accessToken = localStorage.getItem("access-Token");
   https.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
 
-  https.get("/cart").then((response) => {
-    if (response === 200) {
-      console.log("장바구니 목록 가져오기 성공", response);
-      return response;
-    } else {
-      console.log("장바구니 목록 가져오기 실패", response);
-      return response;
-    }
+  return new Promise((resolve) => {
+    https
+      .get("/cart")
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("장바구니 목록 가져오기 성공", response);
+          resolve(response.data);
+        } else {
+          console.log("장바구니 목록 가져오기 실패", response);
+          resolve(response);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   });
 }
 
 // 장바구니에서 삭제하기
-export function delcart(cartDto) {
+export async function delcart(cartDto) {
   const accessToken = localStorage.getItem("access-Token");
   https.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
 
-  https
-    .delete("/cart", {
-      carts_uid: cartDto.carts_uid,
-    })
-    .then((response) => {
-      if (response === 200) {
-        console.log("장바구니에서 삭제 성공", response);
-        return response;
-      } else {
-        console.log("장바구니에서 삭제 실패", response);
-        return response;
-      }
-    });
+  return new Promise((resolve) => {
+    https
+      .delete("/cart", {
+        carts_uid: cartDto.carts_uid,
+      })
+      .then((response) => {
+        if (response === 200) {
+          console.log("장바구니에서 삭제 성공", response);
+          resolve(response.data);
+        } else {
+          console.log("장바구니에서 삭제 실패", response);
+          resolve(response);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
 }
