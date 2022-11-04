c4a16a6795c9e90314e33f13e0b09bcee6b73d9a
dongwoo kim <sunbi5252@naver.com>
2022-11-04 13:53:54 +0900

    :bug: modify 기능 버그 제거, 겹치는 코드 제거

--- backend/src/main/java/com/tasteshopping/product/service/ProductOptionService.java
+++ backend/src/main/java/com/tasteshopping/product/service/ProductOptionService.java
@@ -8,7 +8,5 @@ import com.tasteshopping.product.entity.Products;
 import java.util.List;
 
 public interface ProductOptionService {
-    public ProductOptions createProductOptionList(Products p, String name, String value);
-
-//    List<ProductOptionListDto> getOptionListByUid(int productsUid);
+    ProductOptions createProductOptionList(Products p, String name, String value);
 }
