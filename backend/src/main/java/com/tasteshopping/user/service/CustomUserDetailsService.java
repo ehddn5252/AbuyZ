abe30b8c1da2bfd518300805e59ecef22eabd566
공지훈 <wlgns3914@naver.com>
2022-11-04 11:02:11 +0900

    :bug: withdrawal

--- backend/src/main/java/com/tasteshopping/user/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/tasteshopping/user/service/CustomUserDetailsService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
+import java.util.Optional;
 
 @Component("userDetailsService")
 @Slf4j
@@ -25,10 +26,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(final String email) {
-        // DB에서 유저정보와 권한정보를 가져온다
-        return userRepository.findByEmail(email)
-                .map(users -> createUser(users))
-                .orElseThrow(() -> new UsernameNotFoundException(email + " -> 데이터베이스에서 찾을 수 없습니다."));
+        Optional<Users> findUser =  userRepository.findByEmail(email);
+        if(!findUser.isPresent()||findUser.get().getStatus()==1) throw new UsernameNotFoundException(email + " -> 데이터베이스에서 찾을 수 없습니다.");
+        return createUser(findUser.get());
     }
 
     private org.springframework.security.core.userdetails.User createUser(Users user) {
