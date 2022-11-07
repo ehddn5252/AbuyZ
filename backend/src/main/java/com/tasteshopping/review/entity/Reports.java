d001b452c45d049c8a001194d0074aef5e8d7ac2
박유주 <qkrdbwn98@naver.com>
2022-11-07 11:21:48 +0900

    :sparkles: report entity

--- backend/src/main/java/com/tasteshopping/review/entity/Reports.java
+++ backend/src/main/java/com/tasteshopping/review/entity/Reports.java
@@ -5,8 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -25,4 +28,23 @@ public class Reports {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="reviews_uid")
     private Reviews review;        //리뷰 유아이디
+
+    // 신고 해결 유무 ( 0:대기, 1:거절, 2:승인)
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Float status;
+
+    // 신고 사유 ( 0:허위사실유포, 1:욕설)
+    @Column(nullable = false)
+    private Float reason;
+
+    // 신고 날짜
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date reportDate;
+
+    // 처리 날짜
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date processDate;
 }
