02bd56c7c0d589568a8728a58fcc4670ed805843
DESKTOP-KVCQHCD\SSAFY <tktkd6755@gmail.com>
2022-11-07 12:07:17 +0900

    :fire: cache

--- front/components/payment/KakaoPayment.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function KakaoPayment() {
-  return (
-    <div>
-      <h1> 카카오 결제</h1>
-    </div>
-  );
-}
--- front/prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  semi: false, // 세미클론 사용 안하기
-  tabWidth: 2, // 탭 간격 2칸
-  singleQuote: true, // 작은따옴표 사용
-  useTabs: true, // Tab 띄워쓰기 사용
-};
