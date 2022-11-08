3baea910a767a4823dbd9c44e4c476950463c5d7
박유주 <qkrdbwn98@naver.com>
2022-11-08 12:15:36 +0900

    :fire: API log 지우기

--- front/pages/api/dashboard.js
+++ front/pages/api/dashboard.js
@@ -10,10 +10,8 @@ export async function getDashboardTodo() {
       .get("/dashboard/to-do")
       .then((response) => {
         if (response.status === 200) {
-          console.log("오늘 할일 목록 가져오기 성공", response);
           resolve(response.data);
         } else {
-          console.log("오늘 할일 목록 가져오기 실패", response);
           resolve(response);
         }
       })
@@ -33,10 +31,8 @@ export async function getDashboardDaily() {
       .get("/dashboard/daily")
       .then((response) => {
         if (response.status === 200) {
-          console.log("일자별 요약 가져오기 성공", response);
           resolve(response.data);
         } else {
-          console.log("일자별 요약 가져오기 실패", response);
           resolve(response);
         }
       })
@@ -56,10 +52,8 @@ export async function getDashboardList() {
       .get("/dashboard/list")
       .then((response) => {
         if (response.status === 200) {
-          console.log("리뷰와 고객센터 가져오기 성공", response);
           resolve(response.data);
         } else {
-          console.log("리뷰와 고객센터 가져오기 실패", response);
           resolve(response);
         }
       })
