930638d889d797e096233d9577f425591e44b5cd
공지훈 <wlgns3914@naver.com>
2022-11-11 10:22:58 +0900

    :sparkles: Add uid to inquiry api

--- backend/src/main/java/com/tasteshopping/inquiry/dto/FilteredDto.java
+++ backend/src/main/java/com/tasteshopping/inquiry/dto/FilteredDto.java
@@ -9,6 +9,7 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 public class FilteredDto {
+    private int uid;
     private String status;
     private String category_name;
     private String title;
@@ -18,7 +19,8 @@ public class FilteredDto {
     private String writer;
 
     @QueryProjection
-    public FilteredDto(String status, String category_name, String title, String content, Date start_date,Date end_date,String writer){
+    public FilteredDto(int uid,String status, String category_name, String title, String content, Date start_date,Date end_date,String writer){
+        this.uid = uid;
         this.status = status;
         this.category_name = category_name;
         this.title = title;
