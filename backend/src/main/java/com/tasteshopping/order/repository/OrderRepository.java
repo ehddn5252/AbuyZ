39264d835d7da824ccd0f448844832e2345a4c97
공지훈 <wlgns3914@naver.com>
2022-11-14 15:25:29 +0900

    :bug: Fixed a date check bug when creating an event

--- backend/src/main/java/com/tasteshopping/order/repository/OrderRepository.java
+++ backend/src/main/java/com/tasteshopping/order/repository/OrderRepository.java
@@ -17,7 +17,7 @@ public interface OrderRepository extends JpaRepository<Orders,Integer> {
 
     @EntityGraph(attributePaths = {"review","orderList","inventory","inventory.product","inventory.product.smallCategory",
     "inventory.product.smallCategory.bigCategory"})
-    List<Orders> findByOrderList_DateBetween(Date start_date,Date end_date);
+    List<Orders> findByOrderList_DateBetweenAndStatusNotIn(Date start_date,Date end_date, List status);
 
 //    @Query("select o from Orders o join fetch o.review r where o.orderList=:orderList")
     @Query("select o from Orders o join fetch Inventories i on o.inventory = i  where o.orderList=:orderList")
