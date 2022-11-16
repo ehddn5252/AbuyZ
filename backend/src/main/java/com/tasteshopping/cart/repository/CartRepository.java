bd1728dee12f2845395bb7a20d5a07dc93b84d8f
dongwoo kim <sunbi5252@naver.com>
2022-11-16 15:06:30 +0900

    :white_check_mark: lock 테스트 추가

--- backend/src/main/java/com/tasteshopping/cart/repository/CartRepository.java
+++ backend/src/main/java/com/tasteshopping/cart/repository/CartRepository.java
@@ -4,9 +4,11 @@ import com.tasteshopping.cart.entity.Carts;
 import com.tasteshopping.user.entity.Users;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 
 @Repository
@@ -15,6 +17,7 @@ public interface CartRepository extends JpaRepository<Carts,Integer> {
             "inventory.product.smallCategory","inventory.product.smallCategory.bigCategory"})
     List<Carts> findAll();
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Carts> findByUser(Users user);
 
     @Query(value="select c from Carts c where c.uid = (select max(c2.uid) from Carts c2 where c2.user=:user)")
